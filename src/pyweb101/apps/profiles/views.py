625ec65abbcc7535ff8984571696bccbee179159
Vasyl Dizhak <dijakroot@gmail.com>
2012-11-18 17:22:27 +0200

    View for landing page

--- src/pyweb101/apps/profiles/views.py
+++ src/pyweb101/apps/profiles/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.views.generic.base import TemplateView
+from profiles.models import LandingHypothesisRegistration
+
+
+class LandingPageHypothesisView(TemplateView):
+     template_name = "profiles/langing-page-hypothesis.html"
+
+     def get_context_data(self, **kwargs):
+         context = super(LandingPageHypothesisView, self).get_context_data(**kwargs)
+         context['registered'] = LandingHypothesisRegistration.objects.all().count()
+         return context
--- src/pyweb101/urls.py
+++ src/pyweb101/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from profiles.views import LandingPageHypothesisView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', LandingPageHypothesisView.as_view(), name='landing-page'),
     url(r'^admin/', include(admin.site.urls)),
 )
