4fef680d251c0b18676d1f67f3314f344c222a0c
Vasyl Dizhak <dijakroot@gmail.com>
2012-11-18 18:26:17 +0200

    Few simple tests for landing page

--- src/pyweb101/apps/profiles/tests.py
+++ src/pyweb101/apps/profiles/tests.py
@@ -1,16 +1,22 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from  django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class LangindPageHypothesysTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('landing-page')
+        self.response = self.client.get(self.url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_content_matching(self):
+        self.assertIn('Python WEB development course', self.response.content)
+
+    def test_static_matching(self):
+        self.assertIn('require-jquery.js', self.response.content)
+        self.assertIn('app.css', self.response.content)
+
+    def test_post_request(self):
+        response = self.client.post(self.url)
+        self.assertEquals(response.status_code, 405)
