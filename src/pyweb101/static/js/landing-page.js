b842ed56ac6e528319726c9d96671b17e311f742
rootart <dijakroot@gmail.com>
2012-12-05 12:49:53 +0200

    JS fixes for showing error message in ccase if we user exists

--- src/pyweb101/static/js/landing-page.js
+++ src/pyweb101/static/js/landing-page.js
@@ -17,38 +17,44 @@ define(['jquery', 'underscore', 'backbone', 'backbone-tastypie'],
             el: '.email-form',
             model: InputForm,
             initialize: function(){
-				this.model.on('change', this.render, this);
+                this.model.on('change', this.render, this);
             },
             events: {
                 "click button[type=submit]": "sendData",
                 "blur input[name=email]": "validateData",
             },
-			render: function(){
-				if (!this.model.isNew()){
-					this.$el.html('<p>Thanks we will contact you soon!</p>');
-					this.$el.addClass('success');
-				}
-				if (!this.model.isValid()){
-					this.$('input[type=text]').css({'color': 'red'});
-				}
-				else {
-					this.$('input[type=text]').css({'color': '#BBB'});
-				}
-			},
+            render: function(){
+                if (!this.model.isNew()){
+                    this.$el.html('<p>Thanks we will contact you soon!</p>');
+                    this.$el.addClass('success');
+                }
+                if (!this.model.isValid()){
+                    this.$('input[type=text]').css({'color': 'red'});
+                }
+                else {
+                    this.$('input[type=text]').css({'color': '#BBB'});
+                }
+            },
+            showErrorMessage: function(model, response){
+                this.$('input[type=text]').css({'color': 'red'});
+                this.$('input[type=text]').val('');
+                this.$('input[type=text]').attr('placeholder', 'This email is used!');
+                this.model.clear({'silent': true});
+            },
             sendData: function(){
                 this.model.set('email', $('input[type=text]').val());
                 if (this.model.isValid() && this.model.isNew()){
-                    this.model.save();
+                    this.model.save(this.model.toJSON(), {error: this.showErrorMessage});
                 }
 
                 return false;
             },
-			validateData: function(){
-				this.model.set('email', $('input[type=text]').val());
-				this.render();
-			}
+            validateData: function(){
+                this.model.set('email', $('input[type=text]').val());
+                this.render();
+            }
         });
-        var model = new InputForm();
-        var item = new InputView({model: model});
+        model = new InputForm();
+        item = new InputView({model: model});
     }
 );
